f6d5742d289674c9b93a7ea5a120246eef575f75
Paulo Cunha <paulo.cunha@ahausoftware.com>
2021-05-31 22:31:00 +0000

    Start linq section

---
+++ LINQToObjectsAndQueryOperators/LINQToObjectsAndQueryOperators/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQToObjectsAndQueryOperators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    // Class to manage libraries 
+    class LibraryManager
+    {
+        public List<Library> libraries;
+        public List<Reader> readers;
+    }
+
+    class Library
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public void Print()
+        {
+            Console.WriteLine($"Library {Name} with id {Id}");
+        }
+    }
+
+    class Reader
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Gender { get; set; }
+        public int Age { get; set; }
+
+        // Foreign key
+        public int LibraryId { get; set; }
+
+        public void Print()
+        {
+            Console.WriteLine($"Student {Name} with id {Id}, gender {Gender} " + 
+                $"and age {Age} from the library {LibraryId}");
+        }
+    }
+}
---
+++ linq1/linq1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace linq1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Data source
+            int[] numbers = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+            OddNumbers(numbers);
+            Console.Read();
+        }
+
+        static void OddNumbers(int[] numbers)
+        {
+            Console.WriteLine("Odd numbers: ");
+            IEnumerable<int> oddNumbers = from number in numbers where number % 2 != 0 select number;
+            Console.WriteLine(oddNumbers);
+
+            foreach (int i in oddNumbers)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
