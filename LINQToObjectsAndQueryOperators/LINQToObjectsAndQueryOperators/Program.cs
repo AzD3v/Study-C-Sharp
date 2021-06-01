8bd9d1fdd11acfe83f3c9833f461d68661a738d2
Paulo Cunha <paulo.cunha@ahausoftware.com>
2021-06-01 17:08:47 +0000

    Almost done with linq section

--- LINQToObjectsAndQueryOperators/LINQToObjectsAndQueryOperators/Program.cs
+++ LINQToObjectsAndQueryOperators/LINQToObjectsAndQueryOperators/Program.cs
@@ -10,6 +10,69 @@ namespace LINQToObjectsAndQueryOperators
     {
         static void Main(string[] args)
         {
+            LibraryManager libraryManager = new LibraryManager();
+            // libraryManager.MaleStudents();
+            // libraryManager.FemaleStudents();
+            // libraryManager.SortReadersByAge();
+            // libraryManager.AllReadersFromLibrary1();
+
+            /* int[] someInts = { 30, 12, 4, 3, 12 };
+             IEnumerable<int> sortedInts = from i in someInts orderby i select i;
+             IEnumerable<int> reversedInts = sortedInts.Reverse();
+
+             foreach (int i in reversedInts)
+             {
+                 Console.WriteLine(i);
+             }
+
+             IEnumerable<int> reversedSortedInts = from i in someInts orderby i descending select i;
+
+             foreach (int i in reversedSortedInts)
+             {
+                 Console.WriteLine(i);
+             }*/
+
+            // Get readers from library id 
+            /*string input = Console.ReadLine();
+            try
+            {
+                int inputAsInt = Convert.ToInt32(input);
+                libraryManager.FindReadersByLibraryId(inputAsInt);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Wrong value");
+            }*/
+
+            // Get readers from library id - My solution
+            /* int id = 0;
+
+             Console.WriteLine("Enter id in order to display all readers from the library with that id:");
+             Console.WriteLine("Enter -1 when you're done:");
+
+             do
+             {
+                 ConsoleKeyInfo UserInput = Console.ReadKey();
+
+                 if (char.IsDigit(UserInput.KeyChar))
+                 {
+                     id = int.Parse(UserInput.KeyChar.ToString());
+                     libraryManager.FindReadersByLibraryId(id);
+                     Console.WriteLine("Please choose your next id");
+                 }
+                 else
+                 {
+                     Console.WriteLine("Please enter a valid id");
+                 }
+             // NOT WORKING
+             } while (!(Console.KeyAvailable && Console.ReadKey(true).Key == ConsoleKey.Escape));
+
+             Console.WriteLine("Thanks for using our found reader program!");
+             */
+
+            libraryManager.ReaderAndLibraryNameCollection();
+
+            Console.Read();
         }
     }
 
@@ -18,6 +81,111 @@ namespace LINQToObjectsAndQueryOperators
     {
         public List<Library> libraries;
         public List<Reader> readers;
+
+        // Constructor
+        public LibraryManager()
+        {
+            libraries = new List<Library>();
+            readers = new List<Reader>();
+
+            // Add libraries
+            libraries.Add(new Library { Id = 1, Name = "Library 1" });
+            libraries.Add(new Library { Id = 2, Name = "Library 2" });
+
+            // Add readers
+            readers.Add(new Reader { Id = 1, Name = "Reader A", Gender = "male", Age = 25, LibraryId = 1 });
+            readers.Add(new Reader { Id = 2, Name = "Reader D", Gender = "female", Age = 29, LibraryId = 2 });
+            readers.Add(new Reader { Id = 3, Name = "Reader B", Gender = "male", Age = 21, LibraryId = 1 });
+            readers.Add(new Reader { Id = 4, Name = "Reader C", Gender = "male", Age = 29, LibraryId = 1 });
+            readers.Add(new Reader { Id = 5, Name = "Reader E", Gender = "trans-gender", Age = 31, LibraryId = 2 });
+        }
+
+        public void MaleStudents()
+        {
+            IEnumerable<Reader> maleReaders = from reader in readers where reader.Gender == "male" select reader;
+            Console.WriteLine("Male - Readers: ");
+
+            foreach (Reader maleReader in maleReaders)
+            {
+                maleReader.Print();
+            }
+        }
+
+        public void FemaleStudents()
+        {
+            IEnumerable<Reader> femaleReaders = from reader in readers where reader.Gender == "female" select reader;
+            Console.WriteLine("Female - readers:");
+
+            foreach (Reader femaleReader in femaleReaders)
+            {
+                femaleReader.Print();
+            }
+        }
+
+        public void SortReadersByAge()
+        {
+            var sortedReaders = from reader in readers orderby reader.Age select reader;
+
+            Console.WriteLine("Readers sorted by age:");
+
+            foreach (Reader reader in sortedReaders)
+            {
+                reader.Print();
+            }
+        }
+        
+        public void AllReadersFromLibrary1()
+        {
+            IEnumerable<Reader> library1Readers = from reader in readers
+                                                  join library in libraries
+                                                  on reader.LibraryId equals library.Id
+                                                  where library.Name == "Library 1"
+                                                  select reader;
+
+            Console.WriteLine("Readers from library 1:");
+
+            foreach (Reader reader in library1Readers)
+            {
+                reader.Print();
+            }
+        }
+
+        public void FindReadersByLibraryId(int id)
+        {
+            IEnumerable<Reader> foundReaders = from reader in readers
+                                               join library in libraries
+                                               on reader.LibraryId equals library.Id
+                                               where library.Id == id
+                                               select reader;
+
+            if (foundReaders.Count() == 0)
+            {
+                Console.WriteLine("No readers found");
+                return;
+            }
+
+            Console.WriteLine($"Found readers from library with id: {id}");
+
+            foreach (Reader foundReader in foundReaders)
+            {
+                foundReader.Print();
+            }
+        }
+
+        public void ReaderAndLibraryNameCollection()
+        {
+            var newCollection = from reader in readers
+                                join library in libraries
+                                on reader.LibraryId equals library.Id
+                                orderby reader.Name
+                                select new { ReaderName = reader.Name, LibraryName = library.Name };
+
+            Console.WriteLine("New collection :");
+            foreach (var col in newCollection)
+            {
+                Console.WriteLine($"Reader {col.ReaderName} from library {col.LibraryName}");
+            }
+        }
     }
 
     class Library
---
+++ LinqToSql2/LinqToSql2/MainWindow.xaml.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LinqToSql2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        LinqToSqlDataClassesDataContext dataContext;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            string connectionString = ConfigurationManager.ConnectionStrings["LinqToSql2.Properties.Settings.MyWorkingDBConnectionString"].ConnectionString;
+            dataContext = new LinqToSqlDataClassesDataContext(connectionString);
+
+            GetLibraryOfHenrique();
+        }
+
+        public void InsertLibraries()
+        {
+            dataContext.ExecuteCommand("delete from Library");
+
+            // Create library - instantiation
+            Library library1 = new Library();
+            Library library2 = new Library();
+
+            // Add library content
+            library1.Name = "Biblioteca Cunha";
+            library2.Name = "Biblioteca Municipal";
+
+            // Insert library
+            dataContext.Libraries.InsertOnSubmit(library1);
+            dataContext.Libraries.InsertOnSubmit(library2);
+
+            dataContext.SubmitChanges();
+            MainDataGrid.ItemsSource = dataContext.Libraries;
+        }
+
+        public void InsertReaders()
+        {
+            dataContext.ExecuteCommand("delete from Reader");
+
+            // With a lambda expression we can get a library object directly
+            Library library1 = dataContext.Libraries.First(li => li.Name.Equals("Biblioteca Cunha"));
+            Library library2 = dataContext.Libraries.First(li => li.Name.Equals("Biblioteca Municipal"));
+            // The same as 
+            // "from library in dataContext.Library where library == "Library1" select library"; 
+
+            List<Reader> readers = new List<Reader>();
+
+            readers.Add(new Reader { Name = "Paulo", Gender = "male", LibraryId = library1.Id });
+            readers.Add(new Reader { Name = "Henrique", Gender = "male", Library = library1 });
+            readers.Add(new Reader { Name = "Costa", Gender = "male", Library = library2 });
+            readers.Add(new Reader { Name = "Cunha", Gender = "male", Library = library2 });
+
+            dataContext.Readers.InsertAllOnSubmit(readers);
+            dataContext.SubmitChanges();
+            MainDataGrid.ItemsSource = dataContext.Readers;
+        }
+
+        public void InsertReadingSessions()
+        {
+            dataContext.ExecuteCommand("delete from ReadingSession");
+
+            List<ReadingSession> readingSessions = new List<ReadingSession>();
+
+            readingSessions.Add(new ReadingSession { Name = "Morning session " });
+            readingSessions.Add(new ReadingSession { Name = "Afternoon session " });
+            readingSessions.Add(new ReadingSession { Name = "Evening session " });
+
+            dataContext.ReadingSessions.InsertAllOnSubmit(readingSessions);
+            dataContext.SubmitChanges();
+            MainDataGrid.ItemsSource = dataContext.ReadingSessions;
+        }
+
+        public void InsertReaderSessionAssociations()
+        {
+            Reader Paulo = dataContext.Readers.First(rd => rd.Name == "Paulo");
+            Reader Henrique = dataContext.Readers.First(rd => rd.Name == "Henrique");
+            Reader Costa = dataContext.Readers.First(rd => rd.Name == "Costa");
+            Reader Cunha = dataContext.Readers.First(rd => rd.Name == "Cunha");
+
+            ReadingSession MorningSession = dataContext.ReadingSessions.First(rs => rs.Name.Contains("Morning"));
+            ReadingSession AfternoonSession = dataContext.ReadingSessions.First(rs => rs.Name.Contains("Afternoon"));
+            ReadingSession EveningSession = dataContext.ReadingSessions.First(rs => rs.Name.Contains("Evening"));
+
+            dataContext.ReaderReadingSessions.InsertOnSubmit(new ReaderReadingSession { Reader = Paulo, ReadingSession = EveningSession } );
+
+            ReaderReadingSession rsHenrique = new ReaderReadingSession();
+            rsHenrique.Reader = Henrique;
+            rsHenrique.ReadingSession = MorningSession;
+
+            dataContext.ReaderReadingSessions.InsertOnSubmit(new ReaderReadingSession { Reader = Costa, ReadingSession = AfternoonSession });
+            dataContext.ReaderReadingSessions.InsertOnSubmit(new ReaderReadingSession { Reader = Paulo, ReadingSession = AfternoonSession } );
+            dataContext.ReaderReadingSessions.InsertOnSubmit(rsHenrique);
+
+            dataContext.SubmitChanges();
+            MainDataGrid.ItemsSource = dataContext.ReaderReadingSessions;
+        }
+
+        public void GetLibraryOfHenrique()
+        {
+            Reader Henrique = dataContext.Readers.First(rd => rd.Name == "Paulo");
+            Library HenriquesLibrary = Henrique.Library;
+
+            List<Library> libraries = new List<Library>();
+            libraries.Add(HenriquesLibrary);
+
+            // My solution to get all reading sessions from Henrique
+            /*IEnumerable<ReadingSession> HenriquesReadingSessions = from readingSession in dataContext.ReadingSessions
+                                                      join readerReadingSession in dataContext.ReaderReadingSessions
+                                                      on readingSession.Id equals readerReadingSession.ReadingSessionId
+                                                      where readerReadingSession.ReaderId == Henrique.Id
+                                                      select readingSession;*/
+
+            // Instructor solution to get all reading sessions from Henrique
+            var henriqueLectures = from readingSession in Henrique.ReaderReadingSessions select readingSession.ReadingSession;
+
+            MainDataGrid.ItemsSource = henriqueLectures;
+        }
+
+        public void GetAllReadersFromLibraryMunicipal()
+        {
+            
+        }
+    }
+}
