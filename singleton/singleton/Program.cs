17e37c23904da58b93aac2b418214300e724073c
Paulo Cunha <paulo.cunha@ahausoftware.com>
2021-06-06 22:39:01 +0000

    add singleton

---
+++ singleton/singleton/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Team.Instance.SayHello();
+        }
+    }
+}
---
+++ singleton/singleton/Team.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace singleton
+{
+    class Team
+    {
+        private static Team instance = new Team();
+
+        private Team() { }
+
+        public static Team Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new Team();
+                }
+                return instance;
+            }
+        }
+
+        public void SayHello()
+        {
+            Console.WriteLine("Hello");
+        }
+    }
+}
