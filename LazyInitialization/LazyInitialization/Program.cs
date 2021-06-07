10e9bc24adc43a28d923df4986680dbe598de7aa
Paulo Cunha <paulo.cunha@ahausoftware.com>
2021-06-07 22:02:28 +0000

    Add lazy initialization example

---
+++ LazyInitialization/LazyInitialization/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace LazyInitialization
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            VideoGame videoGame1 = VideoGame.GetVideoGameByIdentifier("Mad Max");
+            VideoGame videoGame2 = VideoGame.GetVideoGameByIdentifier("Assassin's Creed");
+            VideoGame videoGame3 = VideoGame.GetVideoGameByIdentifier("Mad Max");
+
+            Console.WriteLine(Object.ReferenceEquals(videoGame1, videoGame2));
+            Console.WriteLine(Object.ReferenceEquals(videoGame1, videoGame3));
+        }
+    }
+}
---
+++ LazyInitialization/LazyInitialization/VideoGame.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LazyInitialization
+{
+    class VideoGame
+    {
+        // Cache dictionary
+        private static Dictionary<string, VideoGame> _instances = new Dictionary<string, VideoGame>();
+
+        // Fields (including identifier)
+        public string identifier;
+        public string name;
+        public string genre;
+        public float rating;
+
+        // Constructor (private)
+        private VideoGame(string identifier)
+        {
+            this.identifier = identifier;
+        }
+
+        // Method responsable for creating video game objects
+        public static VideoGame GetVideoGameByIdentifier(string identifier)
+        {
+            VideoGame videoGame;
+
+            if (!_instances.ContainsKey(identifier))
+            {
+                videoGame = new VideoGame(identifier);
+                _instances.Add(identifier, videoGame);
+            }
+            else
+            {
+                videoGame = _instances.GetValueOrDefault(identifier);
+            }
+
+            return videoGame;
+        }
+    }
+}
